f3d9924093b68979a74a40bda13169761eb9f2d3
jjfantini <jjman2811@gmail.com>
2024-03-14 09:44:57 -0400

    ♻️ refactor(obb_helpers): added more providers to `get_latest_price()`

--- src/humbldata/core/utils/constants.py
+++ src/humbldata/core/utils/constants.py
@@ -1,1 +1,19 @@
 """A module to contain all project-wide constants."""
+
+from typing import Literal
+
+OBB_EQUITY_PRICE_QUOTE_PROVIDERS = Literal[
+    "cboe", "fmp", "intrinio", "tmx", "tradier", "yfinance"
+]
+
+OBB_EQUITY_PRICE_HISTORICAL_PROVIDERS = Literal[
+    "alpha_vantage",
+    "cboe",
+    "fmp",
+    "intrinio",
+    "polygon",
+    "tiingo",
+    "tmx",
+    "tradier",
+    "yfinance",
+]
