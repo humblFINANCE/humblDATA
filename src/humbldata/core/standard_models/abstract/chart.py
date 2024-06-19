5c9b8cc649727f5724a17b4c984e40e9fe818f13
jenningsFantini <jenningsfantini+github@gmail.com>
2024-06-19 18:05:09 +0100

    ♻️ refactor(rename): changed  to

--- src/humbldata/core/standard_models/abstract/chart.py
+++ src/humbldata/core/standard_models/abstract/chart.py
@@ -4,10 +4,39 @@ from typing import Any, Dict, Optional
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class ChartFormat(str, Enum):
-    """Chart format."""
+class ChartTemplate(str, Enum):
+    """
+    Chart format.
+
+    Available options:
+    - plotly
+    - humbl_light
+    - humbl_dark
+    - plotly_light
+    - plotly_dark
+    - ggplot2
+    - seaborn
+    - simple_white
+    - presentation
+    - xgridoff
+    - ygridoff
+    - gridon
+    - none
+    """
 
     plotly = "plotly"
+    humbl_light = "humbl_light"
+    humbl_dark = "humbl_dark"
+    plotly_light = "plotly_light"
+    plotly_dark = "plotly_dark"
+    ggplot2 = "ggplot2"
+    seaborn = "seaborn"
+    simple_white = "simple_white"
+    presentation = "presentation"
+    xgridoff = "xgridoff"
+    ygridoff = "ygridoff"
+    gridon = "gridon"
+    none = "none"
 
 
 class Chart(BaseModel):
@@ -17,8 +46,8 @@ class Chart(BaseModel):
         default=None,
         description="Raw textual representation of the chart.",
     )
-    format: ChartFormat | None = Field(
-        default=ChartFormat.plotly,
+    theme: ChartTemplate | None = Field(
+        default=ChartTemplate.plotly,
         description="Complementary attribute to the `content` attribute. It specifies the format of the chart.",
     )
     fig: Any | None = Field(
