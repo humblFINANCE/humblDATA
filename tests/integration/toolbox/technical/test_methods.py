6e10f979f46df970713dba587bd919187eeeebd0
jenningsFantini <jenningsfantini+github@gmail.com>
2024-06-21 14:53:04 +0100

    📚 docs(Category: Technical): added docstrings

--- tests/integration/toolbox/technical/test_methods.py
+++ tests/integration/toolbox/technical/test_methods.py
@@ -1,14 +1,29 @@
+"""Testing Integration of passing parameters from Toolbox() to Technical() to MandelbrotChannelFetcher()."""
+
 import pytest
+from src.humbldata.toolbox.technical.technical_controller import Technical
+
+from humbldata.core.standard_models.abstract.humblobject import HumblObject
 from humbldata.core.standard_models.toolbox import ToolboxQueryParams
 from humbldata.core.standard_models.toolbox.technical.mandelbrot_channel import (
     MandelbrotChannelQueryParams,
 )
-from humbldata.core.standard_models.abstract.humblobject import HumblObject
-from src.humbldata.toolbox.technical.technical_controller import Technical
 
 
 @pytest.fixture
 def context_params(request):
+    """
+    Fixture to provide context parameters for the Toolbox.
+
+    Args
+    ----
+        request: The pytest request object containing the parameters.
+
+    Returns
+    -------
+    ToolboxQueryParams:
+        The context parameters for the Toolbox.
+    """
     return ToolboxQueryParams(
         symbol=request.param["symbol"],
         interval=request.param["interval"],
@@ -19,6 +34,19 @@ def context_params(request):
 
 @pytest.fixture
 def command_params(request):
+    """
+    Fixture to provide command parameters for the Mandelbrot Channel.
+
+    Args
+    ----
+        request: The pytest request object containing the parameters.
+
+
+    Returns
+    -------
+    MandelbrotChannelQueryParams:
+        The command parameters for the Mandelbrot Channel.
+    """
     return MandelbrotChannelQueryParams(
         historical=request.param["historical"],
         window=request.param["window"],
@@ -28,6 +56,17 @@ def command_params(request):
 
 @pytest.fixture
 def technical(context_params):
+    """
+    Fixture to provide an instance of the Technical class.
+
+    Args:
+        context_params: The context parameters for the Toolbox.
+
+    Returns
+    -------
+    Technical:
+        An instance of the Technical class.
+    """
     return Technical(context_params)
 
 
@@ -66,6 +105,18 @@ def technical(context_params):
 def test_mandelbrot_channel_integration(
     technical, context_params, command_params
 ):
+    """
+    Test the integration of passing parameters.
+
+    ToolboxQueryParams (context) and MandelbrotChannelQueryParams (command)
+    parameters to the MandelbrotChannelFetcher,
+    and comparing the parameters returned to the ones input.
+
+    Args:
+        technical: The Technical instance.
+        context_params: The context parameters for the Toolbox.
+        command_params: The command parameters for the Mandelbrot Channel.
+    """
     # Act
     result = technical.mandelbrot_channel(**command_params.model_dump())
 
